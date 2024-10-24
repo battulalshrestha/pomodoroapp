9771e285b41be3077b542f45279fd17216ab818a
Nishan shrestha <nishanshrestha@gmail.com>
2024-10-24 03:46:27 -0700

    pomo app

---
+++ models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+# models.py
+class Timers(models.Model):
+    title = models.CharField(max_length=100)
+    hours = models.IntegerField(default=0)
+    minutes = models.IntegerField(default=25)
+    seconds = models.IntegerField(default=0)
+    category = models.CharField(max_length=50)  # Add this line
+    uuid = models.IntegerField(default=0)
+    priority = models.IntegerField(default=0)
